73198c99c6ecd8e590800e4d247c37d655425aa6
rpetersn <ross.peterson@gmail.com>
2016-03-13 09:51:44 -0600

    Adding a "clear:both;" rule to the primary element

--- js/embed-iframe-raw.js
+++ js/embed-iframe-raw.js
@@ -39,7 +39,7 @@ wp3diframewrap.removeChild(divoriganchor); // remove the anchor
 
 // generate some content
 var wp3dmobilecss = "<style>"+
-"#"+params.id+"{ position: relative; padding-bottom: 65%; height: 0; overflow: hidden; max-width: 100%; }"+ 
+"#"+params.id+"{ clear: both; position: relative; padding-bottom: 65%; height: 0; overflow: hidden; max-width: 100%; }"+ 
 "#"+params.id+" iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; margin: 0; }"+
 "@media (max-width: 767px) { #"+params.id+" { padding-bottom: 165%;} }"+
 "@media (min-width: 481px) and (max-width: 767px) and (orientation: landscape) { #"+params.id+" { padding-bottom: 60%;} }"+
